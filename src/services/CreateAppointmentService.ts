4d1cf33114e8cd19762b1f82d25f518066fdf518
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-05-09 22:04:35 -0300

    Creating users/services

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -11,7 +11,7 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
  */
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
@@ -22,7 +22,7 @@ interface Request {
  */
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -36,7 +36,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
