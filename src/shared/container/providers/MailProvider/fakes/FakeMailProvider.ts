ee1512d9f74dba3908169add3173c265bd498c8c
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-07-03 14:56:16 -0300

    email template

--- src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
-import IMailProvider from '../models/IMailProviders';
-
-interface IMessage {
-  to: string;
-  body: string;
-}
+import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 export default class FakeMailProvider implements IMailProvider {
-  private message: IMessage[] = [];
+  private message: ISendMailDTO[] = [];
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.message.push({
-      to,
-      body,
-    });
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.message.push(message);
   }
 }
