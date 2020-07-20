7af2f298a2f0d71823070bbeb3522f7018c230d1
Paulo Felippe <paulofelipperp.dev@gmail.com>
2020-07-20 14:48:41 -0300

    Creating appointment

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -6,22 +6,11 @@ import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-/**
- * [x] Recebimento das informações
- * [x] Tratativa de erros/excessões
- * [x] Acesso ao repositório
- */
-
 interface IRequest {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
-
-/**
- * Dependency Inversion (SOLID): sempre que o service tiver uma dependência
- * externa, em vez de instanciar a classe de repositório dentro do service,
- * receberemos como parâmetro da classe (constructor).
- */
 
 @injectable()
 class CreateAppointmentService {
@@ -30,7 +19,11 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+    user_id,
+  }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
@@ -43,6 +36,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
